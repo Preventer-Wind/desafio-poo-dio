2ae0f66bfeea5c37a635d29ea3db4807f2c48587
Diogo <pradalegion@gmail.com>
2021-08-19 17:49:09 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -20,9 +19,41 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devDiogo = new Dev();
+        devDiogo.setNome("Diogo");
+        devDiogo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devDiogo.getConteudosInscritos());
+        devDiogo.progredir();
+        devDiogo.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos" + devDiogo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devDiogo.getConteudosConcluidos());
+        System.out.println("XP:" +devDiogo.calcularTotalXP());
+
+        System.out.println("-------");
+
+        Dev devThiago = new Dev();
+        devThiago.setNome("Thiago");
+        devThiago.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devThiago.getConteudosInscritos());
+        devThiago.progredir();
+        devThiago.progredir();
+        devThiago.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos" + devThiago.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devThiago.getConteudosConcluidos());
+        System.out.println("XP:" +devThiago.calcularTotalXP());
 
     }
 
